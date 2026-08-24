18d46a2370e7190d15ecad847a1d5c52aad238bc
Linus Larsson <dsub.rp@gmail.com>
2026-08-24 15:55:44 +0200

    rebuild build action on github

--- local_build.sh
+++ local_build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 mkdir -p rpmbuild/src
 
- CI_PROJECT_DIR=`pwd`
- TOP_DIR=$CI_PROJECT_DIR/rpmbuild
+#  CI_PROJECT_DIR=`pwd`
+#  TOP_DIR=$CI_PROJECT_DIR/rpmbuild
+TOP_DIR==`
  BUILD_ROOT=$TOP_DIR/build
  TARGET=./target
  PROJECT_NAME=bashtester
@@ -14,4 +15,4 @@ SOURCE_DIR=$TOP_DIR/src/$PROJECT_NAME-$RPM_VERSION-$RPM_RELEASE/SRC
 mkdir -p $SOURCE_DIR
 cp ./bashtester $SOURCE_DIR/
 
-rpmbuild --buildroot $BUILD_ROOT --define="_topdir $TOP_DIR" --define="_sourcedir $SOURCE_DIR" --define="_rpmdir $TARGET" --define="version $RPM_VERSION" --define="release $RPM_RELEASE" --noclean -bb ./bashtester.spec 
+echo  --buildroot $BUILD_ROOT --define="_topdir $TOP_DIR" --define="_sourcedir $SOURCE_DIR" --define="_rpmdir $TARGET" --define="version $RPM_VERSION" --noclean -bb ./bashtester.spec 
